793fd1ce0f8b72466ea5e531857d8ac1e926ea97
Michyo <michyosyn@gmail.com>
2014-10-13 16:38:20 +0800

    successfully test for one day's Bollinger data.

--- process_data.py
+++ process_data.py
@@ -6,7 +6,6 @@ import datetime
 
 separate_symbol = os.sep
 folder = "data"
-file_name = "20131130.csv"
 
 def filenameIntoDate(file_name):
     file_name_separate = os.path.split(file_name)
@@ -29,7 +28,7 @@ def getNDaysFromDate(date, n):
         return date + datetime.timedelta(days = n)
 
 def dateIntoFilename(date):
-    return time.strftime("%Y%m%d", date)
+    return time.strftime("%Y%m%d", date) + ".csv"
 
 def findLastNDaysFilename(date, n):
     # past_date =  getNDaysFromDate(date, n)
@@ -37,14 +36,98 @@ def findLastNDaysFilename(date, n):
     global folder
     for f in os.listdir(folder):
         file_date = filenameIntoDate(f)
-        print file_date
         if (date - file_date).days in range(1, n):
              file_names.append(f)
     return file_names
 
+''' PASSED TEST CODE
 first = filenameIntoDate("20131104.csv")
 last = filenameIntoDate("20131107.csv")
 print (first - last).days
 
+file_name = "20131130.csv"
 date = filenameIntoDate(file_name)
 print findLastNDaysFilename(date, 20)
+'''
+
+# ---- # ---- # ---- # ---- # ---- # ---- # ---- # ---- # ---- # ----
+
+def getProductCode(file_name):
+    date = filenameIntoDate(file_name)
+    HSIX3_initial_date = filenameIntoDate("20131101.csv")
+    if (date - HSIX3_initial_date).days in range(0, 28):
+        return "HSIX3"
+    HSIZ3_initial_date = filenameIntoDate("20131130.csv")
+    if (date - HSIZ3_initial_date).days in range(0, 30):
+        return "HSIZ3"
+    HSIF4_initial_date = filenameIntoDate("20131231.csv")
+    if (date - HSIF4_initial_date).days in range(0, 30):
+        return "HSIF4"
+    HSIG4_initial_date = filenameIntoDate("20140131.csv")
+    if (date - HSIG4_initial_date).days in range(0, 28):
+        return "HSIG4"
+    HSIH4_initial_date = filenameIntoDate("20130228.csv")
+    if (date - HSIH4_initial_date).days in range(0, 30):
+        return "HSIH4"
+
+def combineFolderWithFilename(file_name):
+    global folder, separate_symbol
+    return folder + separate_symbol + file_name
+
+def getClosePriceForPeriod(files):
+    # return close price from start to end period in an array
+    prices = []
+    for f in files:
+        for line in open(combineFolderWithFilename(f)):
+            data_line = line.split(",")
+            product_code = getProductCode(f)
+            if data_line[1] == product_code and data_line[2] != "999999":
+                prices.append(float(data_line[2]))
+    return prices
+
+# Calculate simple moving average.
+def calcSMA(prices):
+    avg = 0.0
+    if len(prices) == 0:
+        return avg
+    for p in prices:
+        avg += p
+    return avg / len(prices)
+
+# Calculate standard deviation.
+def calcSD(avg, prices):
+    if len(prices) == 0:
+        return 0.0
+    dev = 0.0
+    for data in prices:
+        dev += (data - avg) ** 2
+    return (dev / len(prices)) ** 0.5
+
+def computeOneBollinger(files, bollinger_band_multiplier):
+    prices = getClosePriceForPeriod(files)
+    # print prices
+    middle_bollinger = calcSMA(prices)
+    sd = calcSD(middle_bollinger, prices)
+    print "SD = " + str(sd) # test
+    upper_bollinger = middle_bollinger + bollinger_band_multiplier * sd
+    lower_bollinger = middle_bollinger - bollinger_band_multiplier * sd
+    return [middle_bollinger, upper_bollinger, lower_bollinger]
+
+'''
+def computeBollingerBands(files, N, bollinger_band_multiplier)
+    for i in range(start + N, end):
+        closePrices = getClosePriceForPeriod(files)
+        middleband[i] = calcSMA(closePrices)
+        sd[i] = calcSD(middleband[i], closePrices)
+        upperband[i] = middleband[i] + bollinger_band_multiplier * sd[i]
+        lowerband[i] = middleband[i] - bollinger_band_multiplier * sd[i]
+    return (middleband, upperband, lowerband)
+'''
+
+''' PASSED TEST CODE
+file_group = ['data/20131231.csv', 'data/20131105.csv', 'data/20131106.csv']
+print computeOneBollinger(file_group, 2)
+'''
+
+# ---- # ---- # ---- # ---- # ---- # ---- # ---- # ---- # ---- # ----
+
--- strategy.py
+++ strategy.py
@@ -1,2 +1,12 @@
 __author__ = 'Michyo'
 
+import process_data
+
+file_name = "20131128.csv"
+file_date = process_data.filenameIntoDate(file_name)
+print file_date
+files = process_data.findLastNDaysFilename(file_date, 20)
+print files
+print process_data.computeOneBollinger(files, 2)
+
+
